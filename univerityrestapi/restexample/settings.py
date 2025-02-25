afabd3736459f57d20f61095e8ac6ab9f18c5021
Jake Farrell <jake.farrell88@mail.dcu.ie>
2025-02-25 04:17:19 +0000

    add cors origins, change to slimmer python image

--- univerityrestapi/restexample/settings.py
+++ univerityrestapi/restexample/settings.py
@@ -58,7 +58,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", # node on port 3000
-    "http://127.0.0.1:3000"  # node on port 3000
+    "http://127.0.0.1:3000",  # node on port 3000
+    "http://localhost:80", # CheeseLad - for deployment
+    "https://universitysystem-api.jakefarrell.ie" # CheeseLad - for deployment
 ]
 
 ROOT_URLCONF = 'restexample.urls'
